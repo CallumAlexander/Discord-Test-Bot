44a0ba417bc7d337d99ebb278f5a450eb419a55e
Cal Alexander <s1931801@ed.ac.uk>
2020-03-20 18:06:51 +0000

    Implementation of a very very simple bot that checks if it's ready.
    Signed-off-by: Cal Alexander <s1931801@ed.ac.uk>

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-# 
+# Property of Callum Alexander
+# Bot 0.1 for Discord
+
+from discord.ext import commands
+
+client = commands.Bot(command_prefix=".")
+
+
+@client.event
+async def on_ready():
+    print("Bot is ready")
+
+
+client.run("NjkwNTQ0NTU2Mjk0NDA2MTg0.XnUETQ.HEV7ImbGH0mh8flHLWxj5hhQ5dw")
