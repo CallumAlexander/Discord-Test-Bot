253a2e9f42a0cd0aa402edc69f282aee4cbfc80a
Cal Alexander <s1931801@ed.ac.uk>
2020-03-20 18:15:59 +0000

    Working with very simple event handling - user joining and leaving a server
    Signed-off-by: Cal Alexander <s1931801@ed.ac.uk>

--- main.py
+++ main.py
@@ -11,4 +11,14 @@ async def on_ready():
     print("Bot is ready")
 
 
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined the server')
+
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left the server')
+
+
 client.run("NjkwNTQ0NTU2Mjk0NDA2MTg0.XnUETQ.HEV7ImbGH0mh8flHLWxj5hhQ5dw")
