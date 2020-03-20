44f1b420eb1a6b0c9acd687acfe2a4e25257ab48
Cal Alexander <s1931801@ed.ac.uk>
2020-03-20 19:20:48 +0000

    Basic ping and 8ball commands implemented
    Signed-off-by: Cal Alexander <s1931801@ed.ac.uk>

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # Property of Callum Alexander
 # Bot 0.1 for Discord
 
+import discord
 from discord.ext import commands
+import random
 
 client = commands.Bot(command_prefix=".")
 
@@ -21,4 +23,19 @@ async def on_member_remove(member):
     print(f'{member} has left the server')
 
 
-client.run("NjkwNTQ0NTU2Mjk0NDA2MTg0.XnUETQ.HEV7ImbGH0mh8flHLWxj5hhQ5dw")
+@client.command()
+async def ping(ctx):
+    await ctx.send(f"{client.latency * 1000}ms")
+
+
+@client.command(aliases=['8ball', 'test'])
+async def _8ball(ctx, *, question):
+    responses = [" It is certain.",
+                 "It is decidedly so.",
+                 "Without a doubt.",
+                 "Yes - definitely.",
+                 "You may rely on it.", ]
+    await ctx.send(f"Question : {question} , Answer : {random.choice(responses)}")
+
+
+client.run("NjkwNTQ0NTU2Mjk0NDA2MTg0.XnULdA.X7k5Wp3mKkFxm4T4UR070AfASqc")
