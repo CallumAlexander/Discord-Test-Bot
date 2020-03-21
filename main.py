ce4203823dffc16957563c2d6b72c90fc777abd8
Cal Alexander <s1931801@ed.ac.uk>
2020-03-21 19:51:23 +0000

    Added kick, clear, ban, and on_message functions
    Updated the 8ball function with more messages
    
    Signed-off-by: Cal Alexander <s1931801@ed.ac.uk>

--- main.py
+++ main.py
@@ -5,12 +5,20 @@ import discord
 from discord.ext import commands
 import random
 
-client = commands.Bot(command_prefix=".")
+client = commands.Bot(command_prefix="Hey TestBot ")
 
 
 @client.event
 async def on_ready():
-    print("Bot is ready")
+    print("Bot is up and running")
+
+
+@client.event
+async def on_message(ctx, message):
+    joinResponses = ["Hahahaha I'm back ONLINE",
+                     "Greetings",
+                     "I'm back because I don't hate fun"]
+    await ctx.send(f"{random.choice(joinResponses)}")
 
 
 @client.event
@@ -28,14 +36,44 @@ async def ping(ctx):
     await ctx.send(f"{client.latency * 1000}ms")
 
 
-@client.command(aliases=['8ball', 'test'])
+@client.command(aliases=['8ball', 'eightball'])
 async def _8ball(ctx, *, question):
     responses = [" It is certain.",
                  "It is decidedly so.",
                  "Without a doubt.",
                  "Yes - definitely.",
-                 "You may rely on it.", ]
-    await ctx.send(f"Question : {question} , Answer : {random.choice(responses)}")
+                 "You may rely on it.", "As I see it, yes.",
+                 "Most likely.",
+                 "Outlook good.",
+                 "Yes.",
+                 "Signs point to yes.",
+                 "Reply hazy, try again.",
+                 "Ask again later.",
+                 "Better not tell you now.",
+                 "Cannot predict now.",
+                 "Concentrate and ask again.",
+                 "Don't count on it.",
+                 "My reply is no.",
+                 "My sources say no.",
+                 "Outlook not so good.",
+                 "Very doubtful."]
+    await ctx.send(f"{random.choice(responses)}")
 
 
-client.run("NjkwNTQ0NTU2Mjk0NDA2MTg0.XnULdA.X7k5Wp3mKkFxm4T4UR070AfASqc")
+@client.command()
+async def clear(ctx, amount=5):
+    await ctx.channel.purge(limit=amount)
+
+
+@client.command()
+async def kick(ctx, member: discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+
+
+@client.command()
+async def ban(ctx, member: discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+
+
+# todo - token needed
+client.run("")
